b193088f3718afadc7faddcb68e1986e904e65e5
Ruchira Kalhara Mahaliyana <spiderindustries123@gmail.com>
2020-07-01 14:32:45 +0530

    Adding some service application test cases.

---
+++ src/test/java/com/example/demo/RoleServiceApplicationIntergrationTest.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class RoleServiceApplicationIntergrationTest{
+
+    @Test
+    public void integreated_testing_method_1(){
+
+    }
+}
--- src/test/java/com/example/demo/RoleServiceApplicationTests.java
+++ src/test/java/com/example/demo/RoleServiceApplicationTests.java
@@ -1,13 +1,43 @@
 package com.example.demo;
 
+import com.example.demo.dao.Role_DAO;
+import com.example.demo.dto.Role_DTO;
+import com.example.demo.model.Role;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@SpringBootTest
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+
+@WebMvcTest
 class RoleServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	MockMvc mockMVC;
 
+	@MockBean
+	Role_DAO role_repository;
+
+
+	@Test
+	void contextLoads() throws Exception {
+		when(role_repository.findAll()).thenReturn(Collections.emptyList());
+		MvcResult mvcResult= mockMVC.perform(MockMvcRequestBuilders.get("/Roles/getAllRoles").accept(MediaType.APPLICATION_JSON)).andReturn();
+		System.out.println(mvcResult.getResponse());
+		verify(role_repository).findAll();
+	}
 }
